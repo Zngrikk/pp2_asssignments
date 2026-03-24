dfe60f3be5dd135e2cfdd358e1dc5b1fbcf25c4c
Zngrikk <kambarzangar7@gmail.com>
2026-03-25 02:42:23 +0600

    add new files

--- 2.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-s1 = input()
-
-
-dt1 = datetime.strptime(s1, "%Y-%m-%d")
-days=int(input())
-
-date=dt1+timedelta(days=days)
-print(date.strftime("%Y-%m-%d"))
