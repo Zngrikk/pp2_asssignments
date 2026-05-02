26d2b865edf6b47f08f5ed18daebe50e2f2d3845
Zngrikk <kambarzangar7@gmail.com>
2026-05-02 14:53:24 +0600

    add new files

---
+++ prac08/config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+    "host": "localhost",     
+    "database": "phonebook",  
+    "user": "postgres",     
+    "password": "88776655aa" 
+}DB_CONFIG = {
+    "host": "localhost",     
+    "database": "phonebook",  
+    "user": "postgres",     
+    "password": "88776655aa" 
+}
